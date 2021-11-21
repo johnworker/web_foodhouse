4b4388c5971b430938fe40d158b89704cd76ce38
johnworker <besscomdark@gmail.com>
2021-11-21 21:58:00 +0800

    樣式修改

--- js/cart.js
+++ js/cart.js
@@ -5,31 +5,39 @@ var app = new Vue({
           {
             id:'1',
             itemName:'優質短袖白T',
-            imgUrl:'https://images.unsplash.com/photo-1534961880437-ce5ae2033053?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
-            price:'500',
+            imgUrl:'https://upload.cc/i1/2021/11/21/luoJiR.png',
+            price:'150',
             count:'2'
           },
           {
             id:'2',
             itemName:'骷髏手短黑T',
-            imgUrl:'https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-            price:'790',
+            imgUrl:'https://upload.cc/i1/2021/11/21/2RN5ir.png',
+            price:'150',
             count:'3'
           },
           {
             id:'3',
             itemName:'超時尚牛仔庫',
-            imgUrl:'https://images.unsplash.com/photo-1529391409740-59f2cea08bc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1124&q=80',
-            price:'1200',
+            imgUrl:'https://upload.cc/i1/2021/11/21/jHxpmr.png',
+            price:'200',
             count:'1'
           },
           {
             id:'4',
             itemName:'質感褐色系大衣服',
-            imgUrl:'https://images.unsplash.com/photo-1491998664548-0063bef7856c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-            price:'2300',
+            imgUrl:'https://upload.cc/i1/2021/11/21/OMuNtG.png',
+            price:'250',
             count:'1'
           },
+          {
+            id:'5',
+            itemName:'質感褐色系大衣服',
+            imgUrl:'https://upload.cc/i1/2021/11/21/oBU9vl.png',
+            price:'250',
+            count:'1'
+          },
+
     ]
     },
     methods:{
